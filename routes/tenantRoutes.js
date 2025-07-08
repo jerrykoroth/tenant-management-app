d39ca6ae5649bd8fb24be40e9bbc4376a0784836
Jerry Koroth Jacob <jerrykoroth@gmail.com>
2025-07-09 01:22:04 +1000

    Delete routes directory

--- routes/tenantRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addTenant, getTenants } = require('../controllers/tenantController');
-
-router.post('/tenants', addTenant);
-router.get('/tenants', getTenants);
-
-module.exports = router;
